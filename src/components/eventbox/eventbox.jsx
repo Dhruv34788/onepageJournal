4660b8e0fa2cd409b667a0a28f3554acdf3676c6
dbarochiya <dhruv.barochia34788@gmail.com>
2019-05-01 14:01:50 +0530

    added : eventbox component

---
+++ src/components/eventbox/eventbox.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import './eventbox.css'
+
+class Eventbox extends Component {
+  render(){
+    let className = this.props.data.class
+    let eventinfo = this.props.data.tooltiptext 
+    let style = {
+      background : ''
+    }
+    eventinfo  += this.props.eventdata.map(item  => {
+      if(item){
+        return item.date + ":" + item.name + "\n"
+      }
+    })
+    if(this.props.eventdata.length > 0){
+      if(this.props.eventdata.length > 1){
+        className += 'multi-event '
+      }
+      else{
+        className += 'single-event '
+        style.background = this.props.eventdata[0].color
+      }
+    }
+    console.log('eventbox=rendered');
+    return (
+      <div className={className} style = {style} >
+        <div className="tooltiptext">{eventinfo}</div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  eventdata :  getEventsById(state.events,ownProps.data.events)
+});
+
+const getEventsById = (events , eventIds ) => {
+  let eventdata = eventIds.map(element => {
+    return events[element]
+  })
+  return eventdata
+}
+export default connect(mapStateToProps)(Eventbox);
